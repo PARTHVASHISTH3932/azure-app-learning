3d16f61ee798ab321b2edd032142ba70415c546c
parth3932@outlook.com <parth3932@outlook.com>
2025-02-12 14:43:43 +0530

    added configertaion greetings message

--- azure app learning/Pages/Privacy.cshtml.cs
+++ azure app learning/Pages/Privacy.cshtml.cs
@@ -6,14 +6,17 @@ namespace azure_app_learning.Pages
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
+    
 
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
+           
         }
 
         public void OnGet()
         {
+           
         }
     }
 
