4a5b043b74cd7052a98af8beaa29d8ba238d9845
parth3932@outlook.com <parth3932@outlook.com>
2025-02-13 01:43:50 +0530

    added changes for staging

---
+++ azure app learning/Pages/Deployment.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_app_learning.Pages
+{
+    public class DeploymentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
